2f82f7a0de16de67abc832390e24eada74baeeb8
AlexHloba <108262715+AlexHloba@users.noreply.github.com>
2023-02-19 21:34:57 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.Write("Введите сумму вклада: ");
+decimal vkladSum = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Введите количество лет: ");
+int yearCount = Convert.ToInt32(Console.ReadLine());
+ 
+int x = 1;
+ 
+while (x <= yearCount)
+{
+  if (vkladSum < 100)
+  {
+    vkladSum = vkladSum + (vkladSum * (decimal)0.05);
+  }
+  if ((vkladSum > 100) && (vkladSum < 200))
+ {
+    vkladSum = vkladSum + (vkladSum * (decimal)0.07);
+  }
+  if (vkladSum > 200)
+  {
+        vkladSum = vkladSum + (vkladSum * (decimal)0.1);
+  }
+ 
+  x++;
+}
+ 
+Console.WriteLine("Cумма вклада с начисленными процентами за {0} лет составит: {1}", yearCount, vkladSum);
+
+
